bc12155e747413506951f85cfed5c80ee23f8f46
Aron John Caballe <93106287+FlamingRock2020@users.noreply.github.com>
2022-11-25 15:58:21 +0800

    Add files via upload

---
+++ Caballe_05Laboratory/FrmFileName.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Caballe_05Laboratory
+{
+    partial class FrmFileName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnOkay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(21, 24);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(168, 24);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Enter File Name";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(25, 72);
+            this.txtFileName.Multiline = true;
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(388, 31);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // btnOkay
+            // 
+            this.btnOkay.Location = new System.Drawing.Point(150, 130);
+            this.btnOkay.Name = "btnOkay";
+            this.btnOkay.Size = new System.Drawing.Size(92, 33);
+            this.btnOkay.TabIndex = 2;
+            this.btnOkay.Text = "Okay";
+            this.btnOkay.UseVisualStyleBackColor = true;
+            this.btnOkay.Click += new System.EventHandler(this.btnOkay_Click);
+            // 
+            // FrmFileName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 175);
+            this.Controls.Add(this.btnOkay);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.lblText);
+            this.Name = "FrmFileName";
+            this.Text = "FrmFileName";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnOkay;
+    }
+}
---
+++ Caballe_05Laboratory/FrmRegistration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caballe_05Laboratory
+{
+    public partial class FrmRegistration : Form
+    {
+        public static string userInfo;
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string[] userInfo = {
+                "Student No. :" + txtStudentNo.Text,
+                "Full Name:" + txtLastName.Text + ", " + txtFirstName.Text + ", " + txtMiddleInitial.Text,
+                "Program: " + comboBoxProgram.Text,
+                "Gender: " + comboBoxGender.Text,
+                "Age: " + txtAge.Text,
+                "Birthday :" + dateBirthday.Text,
+                "Contact No." + txtContactNo.Text
+            };
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            FrmFileName filename = new FrmFileName();
+            filename.ShowDialog();
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, FrmFileName.SetFileName)))
+            {
+                foreach (string info in userInfo)
+                {
+                    outputFile.WriteLine(info);
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmStudentRecord studentrecord = new FrmStudentRecord();
+            studentrecord.ShowDialog();
+            Close();
+        }
+    }
+}
